7d8cded37245609c0982445d8d157434e575099f
Sirampy <alex.ch4.cl@gmail.com>
2023-10-22 13:50:35 +0100

    task 3 complete

---
+++ task3/counter_tb.cpp
@@ -0,0 +1,70 @@
+#include "Vcounter.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "vbuddy.cpp"
+#include<iostream>
+
+const char* PROG_NAME = "Lab1: Counter"; // could put all params in a namespace if i end up with a lot of them
+
+int main(int argc, char **argv, char **env){
+    int i; 
+    int clk;
+    int pause = -1;
+    int step = 0;
+
+    Verilated::commandArgs(argc, argv);
+    // init verilog instance
+    Vcounter* top = new Vcounter();
+    //init trace dump
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace (tfp, 99);
+    tfp->open ("counter.vcd");
+
+    // init vbuddy
+    if (vbdOpen()!=1) return (-1);
+    vbdHeader(PROG_NAME);
+    vbdSetMode(1);
+
+    // init simulation inputs
+    top->clk = 1;
+    top->rst = 0;
+    top->ld = 1;
+    top->v = vbdValue();
+
+    // run simulation 
+    for (i = 0; i < 500; i++){
+
+        step = 0;
+        while(step == 0){ // this is cringe - infinite loops W/O timeout are a bad idea - plus there is defo a cleaner way to implement this
+            step = vbdFlag();
+        }
+
+        for (clk = 0; clk < 2; clk++){
+            tfp->dump (2*i+clk);   //unit is in ps
+            top->clk = !top->clk;
+            top->eval ();
+        }
+
+        // send count to vbuddy 
+        
+        vbdHex(4, (int(top->count) >> 16) & 0xF);
+        vbdHex(3, (int(top->count) >> 8) & 0xF);
+        vbdHex(2, (int(top->count) >> 4) & 0xF);
+        vbdHex(1, (int(top->count)) & 0xF);
+        
+        //  current cycle
+        // vbdPlot(int(top->counts), 0, 255);
+        vbdCycle(i+1);
+
+        // change signals during
+        top->ld = 0;
+
+        // exit on finish or stop
+        if (Verilated::gotFinish()) exit(0);
+    }
+
+    vbdClose();
+    tfp -> close();
+    exit(0);
+};
--- task3/readme.md
+++ task3/readme.md
@@ -1,1 +1,32 @@
-Create your design in this folder.
+# Making the counter load value from vbuddy:
+jus as a note, it says modify counter.sv in the instructions when it perhaps should say counter_tb.cpp
+- switch into oneshot mode
+- read value from vbuddy into the v and trigger ld when required:
+```cpp
+    // change signals during
+    shoot = vbdFlag();
+    top->ld = shoot;
+    if(shoot){
+        top->v = vbdValue();
+    }
+    top->rst = 0;
+```
+- turns out i failed at following lab instructions :() and i only need to preset it:
+```cpp
+    // init simulation inputs
+    top->clk = 1;
+    top->rst = 0;
+    top->ld = 1;
+    top->v = vbdValue();
+```
+```cpp
+    // change signals during
+    top->ld = 0;
+```
+# implementing steping only when button is pressed:
+```cpp
+    step = 0;
+    while(step == 0){ // this is cringe - infinite loops W/O timeout are a bad idea - plus there is defo a cleaner way to implement this
+        step = vbdFlag();
+    }
+```
